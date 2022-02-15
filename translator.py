103e85e0a20955242c058a9dcb224d8edec07f2b
avasdowney <avasdowney@gmail.com>
2022-02-14 23:39:56 -0500

    baseline working

--- translator.py
+++ translator.py
@@ -4,4 +4,19 @@ with open(sys.argv[1]) as f:
     # read txt file and convert to readable format
     txt = f.read()
     txt_list = list(txt)
-    print(txt_list)
+
+    # convert chars to ascii equivalent
+    ascii_list = []
+    bf = ">"
+    for x in range(len(txt_list)):
+        ascii_list.append(ord(txt_list[x]))
+
+        # convert ascii values to extremely inefficient brainfuck code
+        for i in range(ascii_list[x]):
+            bf = bf + '+'
+        bf = bf + '.>'
+
+    # write to brainfuck file
+    with open('translation.bf', 'w') as f:
+        f.write(bf)
+
